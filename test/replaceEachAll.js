23a39d7dc8c78ca0f1b6536916c086105f695f60
Aidan Welch <aidan@freedwave.com>
2022-05-11 01:26:27 -0400

    reverse test to correct expected and actual pos

--- test/replaceEachAll.js
+++ test/replaceEachAll.js
@@ -12,31 +12,31 @@ describe("replaceEachAll", () => {
 		it("should replace seperated character matches", () => {
 			const searchValue = " ";
 			const replaceValue = "_";
-			assert.strictEqual(lorem.replaceAll(searchValue, replaceValue), replaceEachAll(lorem, searchValue, replaceValue));
+			assert.strictEqual( replaceEachAll(lorem, searchValue, replaceValue), lorem.replaceAll(searchValue, replaceValue));
 		});
 
 		it("should replace repeating character matches", () => {
 			const searchValue = "a";
 			const replaceValue = "REPLACED";
-			assert.strictEqual(repeatingLetters.replaceAll(searchValue, replaceValue), replaceEachAll(repeatingLetters, searchValue, replaceValue));
+			assert.strictEqual(replaceEachAll(repeatingLetters, searchValue, replaceValue), repeatingLetters.replaceAll(searchValue, replaceValue));
 		});
 
 		it("should replace seperated string matches", () => {
 			const searchValue = "it";
 			const replaceValue = "REPLACED";
-			assert.strictEqual(lorem.replaceAll(searchValue, replaceValue), replaceEachAll(lorem, searchValue, replaceValue));
+			assert.strictEqual(replaceEachAll(lorem, searchValue, replaceValue), lorem.replaceAll(searchValue, replaceValue));
 		});
 
 		it("should not replace on no match", () => {
 			const searchValue = "123456WRONGWRONGWRONG";
 			const replaceValue = "WRONG";
-			assert.strictEqual(lorem.replaceAll(searchValue, replaceValue), replaceEachAll(lorem, searchValue, replaceValue));
+			assert.strictEqual(replaceEachAll(lorem, searchValue, replaceValue), lorem);
 		});
 
 		it("should not replace on match not closed", () => {
 			const searchValue = "aaaaaa";
 			const replaceValue = "WRONG";
-			assert.strictEqual(repeatingLetters.replaceAll(searchValue, replaceValue), replaceEachAll(repeatingLetters, searchValue, replaceValue));
+			assert.strictEqual(replaceEachAll(repeatingLetters, searchValue, replaceValue), repeatingLetters);
 		});
 
 	});
